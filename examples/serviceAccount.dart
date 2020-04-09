aa8c2007d1d1a56edcdfe8aac513ecb33561df2e
Felip Pellicer <felip.pellicer@gmail.com>
2020-04-09 21:55:58 +0200

    Documenting serviceAccount.dart example

--- examples/serviceAccount.dart
+++ examples/serviceAccount.dart
@@ -1,15 +1,24 @@
 import 'dart:io';
-
 import 'package:gcputils/auth_utils.dart';
 import 'package:googleapis/storage/v1.dart' as gcs;
 
+/// Example to show the usage of getClientFromServiceAccount function.
+/// 
+/// In this case, I pass my GCP project-id and my credentials json file as parameters to the main function.
+/// Then, it's possible to use the different apis provided by the googleapis library.
+/// 
+/// For this example I list the buckets contained in the mentioned GCP project.
+/// 
+/// To run this, you can do the following:
+/// 
+/// *dart serviceAccount.dart <project-id> <path-to-credentials-json-file>*
 void main(List<String> args) async {
 
-  const project = "flutter-273418";
+  final project = args[0];
 
   final scopes = [gcs.StorageApi.DevstorageReadOnlyScope];
   
-  final credentials = File(args[0]).readAsStringSync();
+  final credentials = File(args[1]).readAsStringSync();
 
   final client = await getClientFromServiceAccount(credentials, scopes);
 
--- lib/auth_utils.dart
+++ lib/auth_utils.dart
@@ -1,11 +1,14 @@
 import 'package:googleapis_auth/auth_io.dart' as auth;
 
-/// Gets service account client from credentials
-/// [required]
-///   - String credentials: credentials obtained from https://console.cloud.google.com/iam-admin/serviceaccounts
-///   - List<String> scopes: service scopes. For example StorageApi.DevstorageReadOnlyScope, PubsubApi.PubsubScope, etc.
+/// Gets service account client from credentials.
 /// 
-/// See example in https://github.com/fpllcr/gcputils/blob/master/examples/serviceAccount.dart
+/// Required parameters:
+/// 
+/// [credentials] - credentials obtained from https://console.cloud.google.com/iam-admin/serviceaccounts
+/// 
+/// [scopes] - service scopes. For example StorageApi.DevstorageReadOnlyScope, PubsubApi.PubsubScope, etc.
+/// 
+/// See example https://github.com/fpllcr/gcputils/blob/master/examples/serviceAccount.dart
 Future<auth.AutoRefreshingAuthClient> getClientFromServiceAccount(final String credentials, final List<String> scopes) async {
   final accountCredentials = auth.ServiceAccountCredentials.fromJson(credentials);
 
